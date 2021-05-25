a80e321fbd58d0b0873f7b404bdd0642164b3cdf
Ahmed Nabeel <nabeel910@protonmail.com>
2021-05-25 11:32:08 +0200

    update

--- config.def.h
+++ config.def.h
@@ -33,7 +33,7 @@ static const Rule rules[] = {
 	/* class      			instance    title       	tags mask  	  switchtotag  	     isfloating           monitor */
 	{ "Pavucontrol",      		NULL,        NULL,         	0,                  0,  		1,                  -1 },
 	{ "Blueman-manager",      	NULL,        NULL,         	0,                  0,  		1,                  -1 },
-	{ "LibreWolf",        		NULL,        NULL,         	1 ,                 0,  		0,                  -1 },
+	{ "LibreWolf",        		NULL,        NULL,         	0,                  0,  		0,                  -1 },
 	{ "mpv",              		NULL,        NULL,         	1 << 2,             1, 			0,                  -1 },
 	{ "WebApp-AnimeSeries3352",     NULL,        NULL,         	1 << 2,             1,  		0,                  -1 },
 	{ "st-256color",   		NULL,        "pacman-updates",  1 << 8,             1, 			0,                  -1 },
