4782508548106b366bee1460512589e0bd88d193
Ahmed Nabeel <nabeel910@protonmail.com>
2022-01-09 22:03:58 +0200

    initial

---
+++ themes/arc.h
@@ -0,0 +1,6 @@
+//darkpastels(konsole)
+static const char col_gray1[]       = "#404552";
+static const char col_gray2[]       = "#7c818c";
+static const char col_gray3[]       = "#E3DEDE";
+static const char col_gray4[]       = "#FFFFFF";
+static const char col_cyan[]        = "#5294e2";
---
+++ themes/dracula.h
@@ -0,0 +1,5 @@
+static const char col_gray1[]       = "#282a36";
+static const char col_gray2[]       = "#d7d7d7";
+static const char col_gray3[]       = "#ffb86c";
+static const char col_gray4[]       = "#d7d7d7";
+static const char col_cyan[]        = "#725E97";
