a546600a113764b4de99ae7079db8d8e05a6bb4a
Ahmed Nabeel <nabeel910@protonmail.com>
2022-02-13 15:54:00 +0200

    dracula

--- themes/dracula.h
+++ themes/dracula.h
@@ -1,5 +1,5 @@
-static const char col_gray1[]       = "#282a36";
-static const char col_gray2[]       = "#d7d7d7";
-static const char col_gray3[]       = "#ffb86c";
-static const char col_gray4[]       = "#d7d7d7";
-static const char col_cyan[]        = "#725E97";
+static const char col_gray1[]       = "#282A36";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#d7d7d7";
+static const char col_gray4[]       = "#282A36";
+static const char col_cyan[]        = "#bd93f9";
